b5213a7930865945a5e50b053593509b0422ead7
vbocharov <seva.bocharov@mail.ru>
2023-11-05 17:02:57 +0500

    init frontend monorepo

---
+++ frontend/apps/model3d-mobile/jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  displayName: 'model3d-mobile',
+  preset: 'react-native',
+  resolver: '@nx/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  moduleNameMapper: {
+    '\\.svg$': '@nx/react-native/plugins/jest/svg-mock',
+  },
+  coverageDirectory: '../../coverage/apps/model3d-mobile',
+};
